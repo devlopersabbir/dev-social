d83a830ba950138bfeb22c4334a7bf7894e90605
devlopersabbir <devlopersabbir@gmail.com>
2025-03-28 15:44:19 +0600

    file upload

--- app/(tabs)/create.tsx
+++ app/(tabs)/create.tsx
@@ -1,7 +1,11 @@
-import { Text, View } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
 import { useRouter } from "expo-router";
 import { useUser } from "@clerk/clerk-expo";
 import { useState } from "react";
+import { styles } from "@/styles/create.style";
+import { Ionicons } from "@expo/vector-icons";
+import { COLORS } from "@/constants/theme";
+import * as ImagePicker from "expo-image-picker";
 
 export default function Create() {
   const router = useRouter();
@@ -9,6 +13,43 @@ export default function Create() {
   const [caption, setCaption] = useState("");
   const [selectedImage, setSelectedImage] = useState<string | null>("");
   const [loading, setLoading] = useState(false);
+  const [isSharing, setIsSharing] = useState(false);
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ["images", "livePhotos"],
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+    }
+  };
+  if (!selectedImage) {
+    return (
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <TouchableOpacity onPress={() => router.back()}>
+            <Ionicons name="arrow-back" size={28} color={COLORS.primary} />
+          </TouchableOpacity>
+          <Text style={styles.headerTitle}>New Post</Text>
+          <View style={{ width: 28 }} />
+        </View>
+        <TouchableOpacity
+          style={styles.emptyImageContainer}
+          onPress={pickImage}
+        >
+          <Ionicons name="image-outline" size={48} color={COLORS.gray} />
+          <Text style={styles.emptyImageText}>Tab to select and image</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
 
   return (
     <View>
